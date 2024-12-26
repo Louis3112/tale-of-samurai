889f36a8bdf16cdd8e83f0317da4c0dee9df68b4
rayhan889 <raydev1604@gmail.com>
2024-12-26 15:25:59 +0700

    new assets: samurai & enemies

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from sys import exit
 pygame.init()
 
 # Game window screen
-screen_width = 1200
-screen_height = 800
+bottom_panel_height = 150
+screen_width = 800
+screen_height = 400
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height + bottom_panel_height))
 pygame.display.set_caption("Tale of Samurai")
 clock = pygame.time.Clock()
 font_sm = pygame.font.Font("font/Pixeltype.ttf", 24)
@@ -15,7 +16,7 @@ font_sm = pygame.font.Font("font/Pixeltype.ttf", 24)
 running = True
 fps = 30
 
-background_surf = pygame.transform.scale(pygame.image.load("graphics/background.png").convert_alpha(), (screen_width, screen_height))
+background_surf = pygame.transform.scale(pygame.image.load("assets/background.png").convert_alpha(), (screen_width, screen_height))
 test_font_surf = font_sm.render("Hello World!", True, (255, 255, 255))
 
 def draw_bg():
