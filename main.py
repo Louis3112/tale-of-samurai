0297a5f4a65c56c83ab35103455683234e4decdc
rayhan889 <raydev1604@gmail.com>
2024-12-26 12:20:15 +0700

    readjusting screen size

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("Tale of Samurai")
 clock = pygame.time.Clock()
 
 running = True
 fps = 30
 
-background_surf = pygame.transform.scale(pygame.image.load("graphics/background.png"), (800, 400))
+background_surf = pygame.transform.scale(pygame.image.load("graphics/background.png"), (1200, 800))
 
 while running:
     for event in pygame.event.get():
