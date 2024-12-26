ce2be0f39ed13278b8b0e9d49b001cf010c8c826
rayhan889 <raydev1604@gmail.com>
2024-12-26 12:28:40 +0700

    separete the width & height of screen into variables

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 800))
+
+# Game window screen
+screen_width = 1200
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Tale of Samurai")
 clock = pygame.time.Clock()
 font_sm = pygame.font.Font("font/Pixeltype.ttf", 24)
@@ -10,7 +15,7 @@ font_sm = pygame.font.Font("font/Pixeltype.ttf", 24)
 running = True
 fps = 30
 
-background_surf = pygame.transform.scale(pygame.image.load("graphics/background.png"), (1200, 800))
+background_surf = pygame.transform.scale(pygame.image.load("graphics/background.png"), (screen_width, screen_height))
 test_font_surf = font_sm.render("Hello World!", True, (255, 255, 255))
 
 while running:
